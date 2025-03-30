f98b04b8464aa7708d668a6c5c232ee668a50a22
CristianCavanzo <cristian.frontendev@gmail.com>
2025-03-30 09:53:40 -0500

    [Add]: cors

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,23 @@ import transactionsRoutes from '@/modules/transactions/transactions.routes';
 const app = express();
 app.use(express.json());
 
+// cors
+const corstOptions = {
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', corstOptions.origin);
+    res.header('Access-Control-Allow-Methods', corstOptions.methods.join(', '));
+    res.header(
+        'Access-Control-Allow-Headers',
+        corstOptions.allowedHeaders.join(', ')
+    );
+    next();
+});
+
 app.use('/users', userRoutes);
 app.use('/transactions', transactionsRoutes);
 
